f8767356823d5f96dec7e806e3d554dea73edc04
Teguh Pratama <teguh.pratama994@outlook.com>
2023-11-30 08:52:03 +0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "sipantao-44815",
-  url: "https://sipantao-44815.botics.co",
-  api: "https://sipantao-44815.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "sipantao-44815",
+  "url": "https://sipantao-44815.botics.co",
+  "api": "https://sipantao-44815.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
